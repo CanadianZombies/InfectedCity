71adec29b528bca58377eddba3a78b9e9b7c2ac0
David Simmerson <dsimmerson@gmail.com>
2015-02-07 20:24:23 -0500

    Update Server.cpp

--- Code/Server.cpp
+++ Code/Server.cpp
@@ -1,31 +1,5 @@
 #include "Engine.hpp"
 
-class Server {
-  public:
-    Server();
-    ~Server();
-  protected:
-    int mPort;
-    int mControl;
-    
-    int maxedConnection;
-  private:
-    time_t mLastSelect;
-
-    fd_set mRead;
-    fd_set mWrite;
-    fd_set mError;
-    
-    bool mServerOn;
-  public:
-    bool InitiatePort(int portNum);
-    bool ResetPollDescriptors();
-    bool ResetFDControllers();
-    bool SelectAndPoll();
-    
-    void startNewSocket(int ctrl);
-}
-
 Server::Server() {
 
 };
