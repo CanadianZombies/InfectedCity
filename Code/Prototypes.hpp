3ad160753cfe85818ff34f85b2bf6be52abfb813
David Simmerson <dsimmerson@gmail.com>
2015-02-08 21:02:03 -0500

    Update Prototypes.hpp

--- Code/Prototypes.hpp
+++ Code/Prototypes.hpp
@@ -4,4 +4,22 @@
 int ProcessMudFunctions();
 int MainLoop();
 
+const char *Format ( const char *fmt, ... );
+bool SameString ( const std::string &a, const std::string &b );
+bool SameString ( const char *astr, const char *bstr );
+bool SameString ( const char *astr, const std::string bstr );
+bool SameString ( const std::string &astr, const char *bstr );
+bool IsSameList ( const std::string &nameToFind, const std::string &namelist );
+std::string SmashTilde ( std::string str );
+std::string FixAmp ( const std::string &str );
+std::string RemoveChar ( std::string str, char v );
+char* CapitalizeC ( const char *str );
+char *ChopC ( const char *argument, char *first_arg );
+std::string ChopString ( const std::string &argument, std::string &first );
+const char *StripNewLine ( const char *str );
+std::string CapitalSentence ( const std::string &str );
+std::string GetArg ( const std::string &str, int arg );
+std::string GetAfter ( const std::string &str, int arg );
+const char *SmashSpace ( const std::string &str_sm );
+
 #endif
